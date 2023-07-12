e51173a972dd1cbe3fbaca4fed517d65885afc06
Junho Eum <jxe282@gmail.com>
2023-07-12 10:57:11 -0400

    Update pythagorean_expectation_modeling.py

--- main_code/pythagorean_expectation_modeling.py
+++ main_code/pythagorean_expectation_modeling.py
@@ -3,20 +3,18 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
-from preprocess_kbo_data import read_merge_data
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score
 
-
-
-train_df,df_numeric = read_merge_data("kbopitchingdata.csv","kbobattingdata.csv")
-
-def Pythagorean_expectation_modeling(df):
+def Pythagorean_expectation_modeling(df, runsScored, runsAllowed, win_loss_probability):
+    """
+    Pass in average runs scored, runs allowed, win loss probability in a year using feature name from your own dataset.
+    """
     # Create a new column for Pythagorean expectation
-    df['pythag_expect'] = (df['avg_runs_scored'] ** 2) / (df['avg_runs_scored'] ** 2 + df['avg_runs_allowed'] ** 2)
+    df['pythag_expect'] = (df[runsScored] ** 2) / (df[runsScored] ** 2 + df[runsAllowed] ** 2)
 
     # Split the DataFrame into input features and target variable
-    X = df[['avg_runs_scored', 'avg_runs_allowed']]
-    y = df['win_loss_percentage']
+    X = df[[runsScored, runsAllowed]]
+    y = df[win_loss_probability]
 
     # Generate polynomial and interaction features
     poly = PolynomialFeatures(degree=2)
@@ -32,13 +30,49 @@ def Pythagorean_expectation_modeling(df):
     # Predict win_percentage for the test set
     y_pred = model.predict(X_test)
 
-    # Print the model's coefficients
     # Calculate metrics for Pythagorean Expectation model
     mse_pythag = mean_squared_error(y_test, y_pred)
-
     r2_pythag = r2_score(y_test, y_pred)
     print(f'Pythagorean Expectation: MSE = {mse_pythag}, R^2 = {r2_pythag}')
 
 if __name__ == "__main__":
-    train_df = pd.read_csv('./dataset/kbo_train.csv')
-    Pythagorean_expectation_modeling(train_df)
+    # Load your datasets
+    win_loss_prob = pd.read_csv('/Users/junhoeum/Desktop/Summer_23/KBO_analytics/code/team_win_loss_probabilities.csv')
+    teams = pd.read_csv('teams.csv')
+
+    # Create mapping from teamId to teamName
+    team_mapping = teams.set_index('id')['name'].to_dict()
+
+    # Loop through each year
+    for year in range(2018, 2022):  # 2022 is used as the end point as the range function does not include the end point
+        team_boxscore = pd.read_csv(f'/Users/junhoeum/Desktop/Summer_23/KBO_analytics/code/MLB_yearly_boxscore/year_{year}.csv')
+
+        # Ensure that 'year' column matches current year
+        team_boxscore['year'] = int(year)  # the int() function converts the year to an integer
+
+        # Prepare the team boxscore data
+        team_boxscore_grouped = team_boxscore.groupby(['teamId', 'year']).agg({
+            'runsScored': 'sum',
+            'runsPitching': 'sum',  # 'runsPitching' will serve as 'runsAllowed'
+        }).reset_index()
+
+        # Check if team_boxscore_grouped DataFrame is empty
+        if team_boxscore_grouped.empty:
+            print(f'team_boxscore_grouped DataFrame is empty for year {year}.')
+            continue
+
+        # Map teamId to teamName
+        team_boxscore_grouped['teamName'] = team_boxscore_grouped['teamId'].map(team_mapping)
+
+        # Merge with the win-loss probabilities dataset
+        dataset = pd.merge(team_boxscore_grouped, win_loss_prob, left_on=['teamName', 'year'], right_on=['homeName', 'season'])
+
+        # Check if the merged DataFrame is empty
+        if dataset.empty:
+            print(f'Merged DataFrame is empty for year {year}. Check your merge operation.')
+            continue
+
+        dataset.to_csv(f"pythagorean_output_{year}.csv")
+
+        # Run the Pythagorean expectation modeling
+        Pythagorean_expectation_modeling(dataset, 'runsScored', 'runsPitching', 'winProb')
