710a24276a5ee34cac92f478a848715d5dc2b9ab
Junho Eum <jxe282@gmail.com>
2023-07-13 14:48:45 -0400

    Add files via upload

--- Baseball_Analytics_main/Models/poisson_negative_binomial_model.py
+++ Baseball_Analytics_main/Models/poisson_negative_binomial_model.py
@@ -1,29 +1,53 @@
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import numpy as np
 
-def predict_win_probability(df, features, dep_variable):
-    # Define the dependent variable and independent variables
-    y = df[dep_variable]
+# TODO: Fix the code to work with the kbo dataset
+
+def predict_win_probability(df, features):
+    # Define dependent variable
+    if 'total_runs_scored' in df.columns:
+        print("Using 'total_runs_scored' as the dependent variable")
+        y = df['total_runs_scored']
+    else:
+        print("Using 'RBI' as the dependent variable")
+        y = df['RBI']
+        
+    # Define independent variables
+    print("Defining independent variables")
     X = df[features]
-
-    # Add constant to the independent variables matrix
+    
+    # Add a constant to the independent variables matrix
+    print("Adding constant to the independent variables")
     X = sm.add_constant(X)
-
-    # Fit Poisson regression model
-    model = sm.GLM(y, X, family=sm.families.Poisson()).fit()
-
+    
+    # Fit the model
+    print("Fitting the model, this may take a while...")
+    poisson_model = sm.GLM(y, X, family=sm.families.Poisson()).fit()
+    
+    # Predict the win probabilities
+    print("Predicting win probabilities")
+    df['predicted_win_probability'] = poisson_model.predict(X)
+    
+    # Apply the sigmoid transformation to get probabilities between 0 and 1
+    print("Applying sigmoid transformation")
+    df['predicted_win_probability'] = 1 / (1 + np.exp(-df['predicted_win_probability']))
+    
     # Print the model summary
-    print(model.summary())
-
-    # Predict total runs scored
-    df['predicted_runs_scored'] = model.predict(X)
-
-    # Convert runs scored to win probability (this is a simplification, real conversion may be more complex)
-    df['predicted_win_probability'] = df['predicted_runs_scored'] / df[dep_variable]
-
-    # Return DataFrame with predictions
+    print("Model summary:")
+    print(poisson_model.summary())
+    
     return df
+
+
+def plot_predicted_win_probability(df, league_name):
+    plt.hist(df['predicted_win_probability'], bins=20, edgecolor='black')
+    plt.title(f"Distribution of Predicted Win Probabilities - {league_name}")
+    plt.xlabel('Predicted Win Probability')
+    plt.ylabel('Frequency')
+    plt.show()
 
 if __name__ == "__main__":
     # Load the data
@@ -35,7 +59,8 @@ if __name__ == "__main__":
     mlb_features = ['wins', 'losses', 'home', 'avg_runs_scored', 'doubles', 'triples', 'homeruns', 'strikeouts_x', 'bases_on_balls', 'hits_x', 'at_bats']
 
     # Predict win probabilities and print the result
-    df_kbo = predict_win_probability(df_kbo, kbo_features, 'total_runs_scored')
-    df_mlb = predict_win_probability(df_mlb, mlb_features, 'RBI')
-    print(df_kbo)
-    print(df_mlb)
+    df_kbo = predict_win_probability(df_kbo, kbo_features)
+    df_mlb = predict_win_probability(df_mlb, mlb_features)
+    
+    plot_predicted_win_probability(df_kbo, 'KBO')
+    plot_predicted_win_probability(df_mlb, 'MLB')
